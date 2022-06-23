d059280bc3fd819cee427ca715900c7ffed90360
Claus Guttesen <kometen@gmail.com>
2022-06-23 10:01:40 +0200

    Added properpy-logger rather than using lombok's logger.

--- src/main/java/no/difi/statistics/api/IngestRestController.java
+++ src/main/java/no/difi/statistics/api/IngestRestController.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
 import no.difi.statistics.IngestService;
+import no.difi.statistics.PropertyLogger;
 import no.difi.statistics.model.MeasurementDistance;
 import no.difi.statistics.model.TimeSeriesDefinition;
 import no.difi.statistics.model.TimeSeriesPoint;
 import no.difi.statistics.validation.ValidOrgno;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,11 +26,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
-@Slf4j
 @Validated
 @Tag(name = "Statistikk-inndata-api", description = "Legg data inn i statistikk-databasen")
 @RestController
 public class IngestRestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertyLogger.class);
+
     private static final String DIGDIR_ORGNR = "991825827";
     private static final String OWNER_EXPLANATION = "eigar av tidsserien i form av eit organisasjonsnummer";
     private static final String SERIES_NAME_EXPLANATION = "tidsserier finnes ved oppslag i /meta";
