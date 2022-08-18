d4ac0425eed91c38bf51c5f12a10ba6e23e1f7e3
Claus Guttesen <kometen@gmail.com>
2022-08-18 10:49:06 +0200

    AOS-166 Migrate from javax.xml.* to jakarta.xml.*. (#32)
    Migrate from javax.xml.* to jakarta.xml.*.

--- src/main/java/no/difi/statistics/client/model/IngestResponse.java
+++ src/main/java/no/difi/statistics/client/model/IngestResponse.java
@@ -1,7 +1,8 @@
 package no.difi.statistics.client.model;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
 import java.util.ArrayList;
 import java.util.List;
 
