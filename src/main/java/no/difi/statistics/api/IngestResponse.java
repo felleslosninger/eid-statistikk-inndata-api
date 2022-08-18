f92ec6a9392882ec05d7ca37c04e42d06af5f13f
Claus Guttesen <kometen@gmail.com>
2022-08-18 13:29:19 +0200

    Removed jakarta from pom.xml, otherwise swagger failed to load.

--- src/main/java/no/difi/statistics/api/IngestResponse.java
+++ src/main/java/no/difi/statistics/api/IngestResponse.java
@@ -1,8 +1,7 @@
 package no.difi.statistics.api;
 
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
-
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
